9a77ca0d0fad26db39a830adb4d76797563f1142
brianc <brian.m.carlson@gmail.com>
2013-03-23 14:20:24 -0500

    make delete take where clause directly

--- test/dialects/delete-tests.js
+++ test/dialects/delete-tests.js
@@ -10,3 +10,28 @@ Harness.test({
   mysql : 'DELETE FROM `post` WHERE (`post`.`content` = ?)',
   params: ['']
 });
+
+Harness.test({
+  query : post.delete().where({content: ''}),
+  pg    : 'DELETE FROM "post" WHERE ("post"."content" = $1)',
+  sqlite: 'DELETE FROM "post" WHERE ("post"."content" = $1)',
+  mysql : 'DELETE FROM `post` WHERE (`post`.`content` = ?)',
+  params: ['']
+});
+
+Harness.test({
+  query : post.delete({content: ''}),
+  pg    : 'DELETE FROM "post" WHERE ("post"."content" = $1)',
+  sqlite: 'DELETE FROM "post" WHERE ("post"."content" = $1)',
+  mysql : 'DELETE FROM `post` WHERE (`post`.`content` = ?)',
+  params: ['']
+});
+
+Harness.test({
+  query : post.delete({content: ''}).or(post.content.isNull()),
+  pg    : 'DELETE FROM "post" WHERE (("post"."content" = $1) OR ("post"."content" IS NULL))',
+  sqlite: 'DELETE FROM "post" WHERE (("post"."content" = $1) OR ("post"."content" IS NULL))',
+  mysql : 'DELETE FROM `post` WHERE ((`post`.`content` = ?) OR (`post`.`content` IS NULL))',
+  params: ['']
+});
+
