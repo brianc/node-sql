c32b00b99b0332fa19ae32a4e3b73b5b8e3340ee
Eric Sessoms <eric@nubgames.com>
2013-03-29 17:22:04 -0400

    added more tests of clause ordering

--- test/dialects/clause-ordering-tests.js
+++ test/dialects/clause-ordering-tests.js
@@ -20,3 +20,24 @@ Harness.test({
   mysql : 'SELECT `user`.`id` FROM `user` WHERE (`user`.`name` = ?)',
   params: ['']
 });
+
+//SELECT - FROM - WHERE
+Harness.test({
+  query : user
+            .select(user.name, post.content)
+            .from(user.join(post).on(user.id.equals(post.userId)))
+            .where({ name: '' }),
+  pg    : 'SELECT "user"."name", "post"."content" FROM "user" INNER JOIN "post" ON ("user"."id" = "post"."userId") WHERE ("user"."name" = $1)',
+  sqlite: 'SELECT "user"."name", "post"."content" FROM "user" INNER JOIN "post" ON ("user"."id" = "post"."userId") WHERE ("user"."name" = $1)',
+  mysql : 'SELECT `user`.`name`, `post`.`content` FROM `user` INNER JOIN `post` ON (`user`.`id` = `post`.`userId`) WHERE (`user`.`name` = ?)',
+  params: ['']
+});
+
+//SELECT - FROM - WHERE
+Harness.test({
+  query : user.select(user.id).from(user).where({name: ''}),
+  pg    : 'SELECT "user"."id" FROM "user" WHERE ("user"."name" = $1)',
+  sqlite: 'SELECT "user"."id" FROM "user" WHERE ("user"."name" = $1)',
+  mysql : 'SELECT `user`.`id` FROM `user` WHERE (`user`.`name` = ?)',
+  params: ['']
+});
