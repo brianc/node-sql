0bd0ee7207fb0651b5583ac9a597f6936337d83d
Sascha Depold <sascha@depold.com>
2013-05-19 21:46:44 +0200

    added methods for reading indexes

---
+++ test/dialects/indexes-tests.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Harness = require('./support');
+var post = Harness.definePostTable();
+
+Harness.test({
+  query:  post.indexes(),
+  pg:     "SELECT relname FROM pg_class WHERE oid IN ( SELECT indexrelid FROM pg_index, pg_class WHERE pg_class.relname=\"post\" AND pg_class.oid=pg_index.indrelid)",
+  mysql:  "SHOW INDEX FROM `post`",
+  sqlite: "PRAGMA INDEX_LIST(\"post\")"
+});
