8cba3ac7bc22ba94c22a6c8b3c15ccd66000e341
Di Wu <dwu@palantir.com>
2013-06-10 14:08:09 -0700

    fix up comment in unary, fix up delete-tests

--- test/dialects/delete-tests.js
+++ test/dialects/delete-tests.js
@@ -4,7 +4,7 @@ var Harness = require('./support');
 var post = Harness.definePostTable();
 
 Harness.test({
-  query : post['delete']().where(post.content.equals('')),
+  query : post.delete().where(post.content.equals('')),
   pg    : 'DELETE FROM "post" WHERE ("post"."content" = $1)',
   sqlite: 'DELETE FROM "post" WHERE ("post"."content" = $1)',
   mysql : 'DELETE FROM `post` WHERE (`post`.`content` = ?)',
@@ -12,7 +12,7 @@ Harness.test({
 });
 
 Harness.test({
-  query : post['delete']().where({content: ''}),
+  query : post.delete().where({content: ''}),
   pg    : 'DELETE FROM "post" WHERE ("post"."content" = $1)',
   sqlite: 'DELETE FROM "post" WHERE ("post"."content" = $1)',
   mysql : 'DELETE FROM `post` WHERE (`post`.`content` = ?)',
@@ -20,7 +20,7 @@ Harness.test({
 });
 
 Harness.test({
-  query : post['delete']({content: ''}),
+  query : post.delete({content: ''}),
   pg    : 'DELETE FROM "post" WHERE ("post"."content" = $1)',
   sqlite: 'DELETE FROM "post" WHERE ("post"."content" = $1)',
   mysql : 'DELETE FROM `post` WHERE (`post`.`content` = ?)',
@@ -28,7 +28,7 @@ Harness.test({
 });
 
 Harness.test({
-  query : post['delete']({content: ''}).or(post.content.isNull()),
+  query : post.delete({content: ''}).or(post.content.isNull()),
   pg    : 'DELETE FROM "post" WHERE (("post"."content" = $1) OR ("post"."content" IS NULL))',
   sqlite: 'DELETE FROM "post" WHERE (("post"."content" = $1) OR ("post"."content" IS NULL))',
   mysql : 'DELETE FROM `post` WHERE ((`post`.`content` = ?) OR (`post`.`content` IS NULL))',
