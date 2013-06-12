65da483945f9fa4c30349214b8dee8244531c540
Brian Carlson <brian.m.carlson@gmail.com>
2013-06-12 09:56:47 -0500

    Add valueExpressionMixin to ColumnNode

--- test/dialects/extra-column-text-tests.js
+++ test/dialects/extra-column-text-tests.js
@@ -4,8 +4,8 @@ var Harness = require('./support');
 var post = Harness.definePostTable();
 
 Harness.test({
-  query : post.select(post.userId.appendText('::string')).where(post.userId.appendText(' IS NOT NULL')),
-  pg    : 'SELECT "post"."userId"::string FROM "post" WHERE "post"."userId" IS NOT NULL',
-  sqlite: 'SELECT "post"."userId"::string FROM "post" WHERE "post"."userId" IS NOT NULL',
-  mysql : 'SELECT `post`.`userId`::string FROM `post` WHERE `post`.`userId` IS NOT NULL'
+  query : post.select(post.userId.appendText('::string')).where(post.userId.appendText('  ').equals('1')),
+  pg    : 'SELECT "post"."userId"::string FROM "post" WHERE ("post"."userId"   = $1)',
+  sqlite: 'SELECT "post"."userId"::string FROM "post" WHERE ("post"."userId"   = $1)',
+  mysql : 'SELECT `post`.`userId`::string FROM `post` WHERE (`post`.`userId`   = ?)'
 });
