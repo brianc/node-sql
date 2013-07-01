5f9413d26a432f1754fce88d762ec6fdd3e218b8
Di Wu <dw323@cornell.edu>
2013-06-30 20:43:50 -0700

    making CreateIndexNode immutable

--- test/immutability-tests.js
+++ test/immutability-tests.js
@@ -1,13 +1,57 @@
-/* global test */
+/* global describe, it */
 'use strict';
 var assert = require('assert');
 
 var ColumnNode = require('../lib/node/column');
+var CreateIndexNode = require('../lib/node/createIndex');
 
-test('ColumNode immutable', function() {
+describe('ColumnNode', function() {
   var original = new ColumnNode({});
-  var aliased = original.as('something else');
+  var originalJSON = JSON.stringify(original);
+  var checkUnchanged = function() {
+    assert.equal(JSON.stringify(original), originalJSON);
+  };
 
-  assert.notEqual(original, aliased);
-  assert.equal(aliased.alias, 'something else');
+  it('#as immutable', function() {
+    assert.notEqual(original, original.as(''));
+    checkUnchanged();
+  });
+});
+
+describe('CreateIndexNode', function() {
+  var original = new CreateIndexNode('table', 'name');
+  var originalJSON = JSON.stringify(original);
+  var checkUnchanged = function() {
+    assert.equal(JSON.stringify(original), originalJSON);
+  };
+
+  it('#unique immutable', function() {
+    assert.notEqual(original, original.unique());
+    checkUnchanged();
+  });
+
+  it('#spatial immutable', function() {
+    assert.notEqual(original, original.spatial());
+    checkUnchanged();
+  });
+
+  it('#fulltext immutable', function() {
+    assert.notEqual(original, original.fulltext());
+    checkUnchanged();
+  });
+
+  it('#using immutable', function() {
+    assert.notEqual(original, original.using(''));
+    checkUnchanged();
+  });
+
+  it('#on immutable', function() {
+    assert.notEqual(original, original.on(''));
+    checkUnchanged();
+  });
+
+  it('#withParser immutable', function() {
+    assert.notEqual(original, original.withParser(''));
+    checkUnchanged();
+  });
 });
