dbad8c5ab4c007323ddbac2b2de035a9e8fd7f5c
Gerasimos Athanasopoulos <ath.ger@gmail.com>
2014-03-27 00:17:57 +0200

    [ADD] tests for for share

--- test/dialects/for-share-tests.js
+++ test/dialects/for-share-tests.js
@@ -2,17 +2,22 @@
 
 var Harness = require('./support');
 var post = Harness.definePostTable();
-var bar = Harness.definePostTable();
+var user = Harness.defineUserTable();
 
 Harness.test({
-  query: post.select(post.star()).forUpdate(),
+  query: post.select(post.star()).forShare(),
   pg: {
-    text  : 'SELECT "post".* FROM "post" FOR UPDATE',
-    string: 'SELECT "post".* FROM "post" FOR UPDATE'
-  },
-  mysql: {
-    text  : 'SELECT `post`.* FROM `post` FOR UPDATE',
-    string: 'SELECT `post`.* FROM `post` FOR UPDATE'
+    text  : 'SELECT "post".* FROM "post" FOR SHARE',
+    string: 'SELECT "post".* FROM "post" FOR SHARE'
   },
   params: []
+});
+
+Harness.test({
+  query: post.select(post.star()).from(post.join(user).on(user.id.equals(post.userId))).where(post.content.equals('foo')).forShare(),
+  pg: {
+    text  : 'SELECT "post".* FROM "post" INNER JOIN "user" ON ("user"."id" = "post"."userId") WHERE ("post"."content" = $1) FOR SHARE',
+    string: 'SELECT "post".* FROM "post" INNER JOIN "user" ON ("user"."id" = "post"."userId") WHERE ("post"."content" = \'foo\') FOR SHARE'
+  },
+  params: ["foo"]
 });
