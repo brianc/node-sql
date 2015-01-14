728fe06afd72424904fb20a3a9f8ccbcb8e72c19
Bergwinkl Thomas <bergwinkl@bulwiengesa.de>
2015-01-14 10:13:28 +0100

    fixed missing table name for .star() with alias case

--- test/dialects/select-tests.js
+++ test/dialects/select-tests.js
@@ -2,6 +2,7 @@
 
 var Harness = require('./support');
 var post = Harness.definePostTable();
+var customerAlias = Harness.defineCustomerAliasTable();
 
 Harness.test({
   query: post.select(post.id).select(post.content),
@@ -16,6 +17,23 @@ Harness.test({
   mysql: {
     text  : 'SELECT `post`.`id`, `post`.`content` FROM `post`',
     string: 'SELECT `post`.`id`, `post`.`content` FROM `post`'
+  },
+  params: []
+});
+
+Harness.test({
+  query: customerAlias.select(customerAlias.star()),
+  pg: {
+    text  : 'SELECT "customer"."id" AS "id_alias", "customer"."name" AS "name_alias", "customer"."age" AS "age_alias", "customer"."income" AS "income_alias", "customer"."metadata" AS "metadata_alias" FROM "customer"',
+    string: 'SELECT "customer"."id" AS "id_alias", "customer"."name" AS "name_alias", "customer"."age" AS "age_alias", "customer"."income" AS "income_alias", "customer"."metadata" AS "metadata_alias" FROM "customer"'
+  },
+  sqlite: {
+    text  : 'SELECT "customer"."id" AS "id_alias", "customer"."name" AS "name_alias", "customer"."age" AS "age_alias", "customer"."income" AS "income_alias", "customer"."metadata" AS "metadata_alias" FROM "customer"',
+    string: 'SELECT "customer"."id" AS "id_alias", "customer"."name" AS "name_alias", "customer"."age" AS "age_alias", "customer"."income" AS "income_alias", "customer"."metadata" AS "metadata_alias" FROM "customer"'
+  },
+  mysql: {
+    text :  'SELECT `customer`.`id` AS `id_alias`, `customer`.`name` AS `name_alias`, `customer`.`age` AS `age_alias`, `customer`.`income` AS `income_alias`, `customer`.`metadata` AS `metadata_alias` FROM `customer`',
+    string: 'SELECT `customer`.`id` AS `id_alias`, `customer`.`name` AS `name_alias`, `customer`.`age` AS `age_alias`, `customer`.`income` AS `income_alias`, `customer`.`metadata` AS `metadata_alias` FROM `customer`'
   },
   params: []
 });
