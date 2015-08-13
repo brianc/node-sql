da6150eefe8ffd22117a825790696fa2badf2b4f
Eduardo Dutra <emdutra@gmail.com>
2015-08-13 16:55:01 -0300

    Addes Create View Support - closes #245

---
+++ test/dialects/create-view-tests.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var Harness = require('./support');
+var user = Harness.defineUserTable();
+
+//simple view create
+Harness.test({
+  query: user.select(user.star()).createView('allUsersView'),
+  pg: {
+    text  : 'CREATE VIEW "allUsersView" AS SELECT "user".* FROM "user"',
+    string: 'CREATE VIEW "allUsersView" AS SELECT "user".* FROM "user"'
+  },
+  sqlite: {
+    text  : 'CREATE VIEW "allUsersView" AS SELECT "user".* FROM "user"',
+    string: 'CREATE VIEW "allUsersView" AS SELECT "user".* FROM "user"'
+  },
+  mysql: {
+    text  : 'CREATE VIEW `allUsersView` AS SELECT `user`.* FROM `user`',
+    string: 'CREATE VIEW `allUsersView` AS SELECT `user`.* FROM `user`'
+  },
+  mssql: {
+    text  : 'CREATE VIEW [allUsersView] AS SELECT [user].* FROM [user]',
+    string: 'CREATE VIEW [allUsersView] AS SELECT [user].* FROM [user]'
+  },
+  oracle: {
+    text  : 'CREATE VIEW "allUsersView" AS SELECT "user".* FROM "user"',
+    string: 'CREATE VIEW "allUsersView" AS SELECT "user".* FROM "user"'
+  }
+});
+
+//create view with parameters
+Harness.test({
+  query: user.select(user.star()).where(user.id.equals(1)).createView('oneUserView'),
+  pg: {
+    text  : 'CREATE VIEW "oneUserView" AS SELECT "user".* FROM "user" WHERE ("user"."id" = 1)',
+    string: 'CREATE VIEW "oneUserView" AS SELECT "user".* FROM "user" WHERE ("user"."id" = 1)'
+  },
+  sqlite: {
+    text  : 'CREATE VIEW "oneUserView" AS SELECT "user".* FROM "user" WHERE ("user"."id" = 1)',
+    string: 'CREATE VIEW "oneUserView" AS SELECT "user".* FROM "user" WHERE ("user"."id" = 1)'
+  },
+  mysql: {
+    text  : 'CREATE VIEW `oneUserView` AS SELECT `user`.* FROM `user` WHERE (`user`.`id` = 1)',
+    string: 'CREATE VIEW `oneUserView` AS SELECT `user`.* FROM `user` WHERE (`user`.`id` = 1)'
+  },
+  mssql: {
+    text  : 'CREATE VIEW [oneUserView] AS SELECT [user].* FROM [user] WHERE ([user].[id] = 1)',
+    string: 'CREATE VIEW [oneUserView] AS SELECT [user].* FROM [user] WHERE ([user].[id] = 1)'
+  },
+  oracle: {
+    text  : 'CREATE VIEW "oneUserView" AS SELECT "user".* FROM "user" WHERE ("user"."id" = 1)',
+    string: 'CREATE VIEW "oneUserView" AS SELECT "user".* FROM "user" WHERE ("user"."id" = 1)'
+  }
+});
