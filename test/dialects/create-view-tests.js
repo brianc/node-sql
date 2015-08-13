ac98e770a3be8037531b75684de5fce2c9d1da79
Eduardo Dutra <emdutra@gmail.com>
2015-08-13 17:34:20 -0300

    Added tests for error raised in non-SELECT create view attempts

--- test/dialects/create-view-tests.js
+++ test/dialects/create-view-tests.js
@@ -52,3 +52,29 @@ Harness.test({
     string: 'CREATE VIEW "oneUserView" AS SELECT "user".* FROM "user" WHERE ("user"."id" = 1)'
   }
 });
+
+//Tests error raised for non-SELECT create view attempts
+Harness.test({
+  query: user.delete().where(user.id.equals(1)).createView('oneUserView'),
+  pg: {
+    text  : 'Create View requires a Select.',
+    throws: true
+  },
+  sqlite: {
+    text  : 'Create View requires a Select.',
+    throws: true
+  },
+  mysql: {
+    text  : 'Create View requires a Select.',
+    throws: true
+  },
+  mssql: {
+    text  : 'Create View requires a Select.',
+    throws: true
+  },
+  oracle: {
+    text  : 'Create View requires a Select.',
+    throws: true
+  },
+  params: []
+});
