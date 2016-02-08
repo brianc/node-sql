775e93829a5a0d57c5e0d22302fd04cac9d8764b
Dan Rzeppa <danrzeppa@pobox.com>
2016-02-08 13:10:32 -0500

    Extend query with value expressions to create allow creating select statements that can be joined together with expressions.

--- test/dialects/select-tests.js
+++ test/dialects/select-tests.js
@@ -3,6 +3,7 @@
 var Harness = require('./support');
 var post = Harness.definePostTable();
 var customerAlias = Harness.defineCustomerAliasTable();
+var Sql = require('../../lib');
 
 Harness.test({
   query: post.select(post.id).select(post.content),
@@ -50,6 +51,107 @@ Harness.test({
   oracle: {
     text  : 'SELECT "customer"."id" "id_alias", "customer"."name" "name_alias", "customer"."age" "age_alias", "customer"."income" "income_alias", "customer"."metadata" "metadata_alias" FROM "customer"',
     string: 'SELECT "customer"."id" "id_alias", "customer"."name" "name_alias", "customer"."age" "age_alias", "customer"."income" "income_alias", "customer"."metadata" "metadata_alias" FROM "customer"'
+  },
+  params: []
+});
+
+// Test that we can generate a SELECT claus without a FROM clause
+Harness.test({
+  query: Sql.select(),
+  pg: {
+    text  : 'SELECT ',
+    string: 'SELECT '
+  },
+  sqlite: {
+    text  : 'SELECT ',
+    string: 'SELECT '
+  },
+  mysql: {
+    text  : 'SELECT ',
+    string: 'SELECT '
+  },
+  mssql: {
+    text  : 'SELECT ',
+    string: 'SELECT '
+  },
+  oracle: {
+    text  : 'SELECT ',
+    string: 'SELECT '
+  },
+  params: []
+});
+
+Harness.test({
+  query: Sql.select("1").where("1=1"),
+  pg: {
+    text  : 'SELECT 1 WHERE (1=1)',
+    string: 'SELECT 1 WHERE (1=1)'
+  },
+  sqlite: {
+    text  : 'SELECT 1 WHERE (1=1)',
+    string: 'SELECT 1 WHERE (1=1)'
+  },
+  mysql: {
+    text  : 'SELECT 1 WHERE (1=1)',
+    string: 'SELECT 1 WHERE (1=1)'
+  },
+  mssql: {
+    text  : 'SELECT 1 WHERE (1=1)',
+    string: 'SELECT 1 WHERE (1=1)'
+  },
+  oracle: {
+    text  : 'SELECT 1 WHERE (1=1)',
+    string: 'SELECT 1 WHERE (1=1)'
+  },
+  params: []
+});
+
+Harness.test({
+  query: Sql.select(post.select(post.id)),
+  pg: {
+    text  : 'SELECT (SELECT "post"."id" FROM "post")',
+    string: 'SELECT (SELECT "post"."id" FROM "post")'
+  },
+  sqlite: {
+    text  : 'SELECT (SELECT "post"."id" FROM "post")',
+    string: 'SELECT (SELECT "post"."id" FROM "post")'
+  },
+  mysql: {
+    text  : 'SELECT (SELECT `post`.`id` FROM `post`)',
+    string: 'SELECT (SELECT `post`.`id` FROM `post`)'
+  },
+  mssql: {
+    text  : 'SELECT (SELECT [post].[id] FROM [post])',
+    string: 'SELECT (SELECT [post].[id] FROM [post])'
+  },
+  oracle: {
+    text  : 'SELECT (SELECT "post"."id" FROM "post")',
+    string: 'SELECT (SELECT "post"."id" FROM "post")'
+  },
+  params: []
+});
+
+Harness.test({
+  query: Sql.select(post.select(post.content).plus(post.select(post.content))),
+  pg: {
+    text  : 'SELECT ((SELECT "post"."content" FROM "post") + (SELECT "post"."content" FROM "post"))',
+    string: 'SELECT ((SELECT "post"."content" FROM "post") + (SELECT "post"."content" FROM "post"))'
+  },
+  sqlite: {
+    text  : 'SELECT ((SELECT "post"."content" FROM "post") + (SELECT "post"."content" FROM "post"))',
+    string: 'SELECT ((SELECT "post"."content" FROM "post") + (SELECT "post"."content" FROM "post"))'
+  },
+  mysql: {
+    text  : 'SELECT ((SELECT `post`.`content` FROM `post`) + (SELECT `post`.`content` FROM `post`))',
+    string: 'SELECT ((SELECT `post`.`content` FROM `post`) + (SELECT `post`.`content` FROM `post`))'
+  },
+  mssql: {
+    text  : 'SELECT ((SELECT [post].[content] FROM [post]) + (SELECT [post].[content] FROM [post]))',
+    string: 'SELECT ((SELECT [post].[content] FROM [post]) + (SELECT [post].[content] FROM [post]))'
+  },
+  oracle: {
+    text  : 'SELECT ((SELECT "post"."content" FROM "post") + (SELECT "post"."content" FROM "post"))',
+    string: 'SELECT ((SELECT "post"."content" FROM "post") + (SELECT "post"."content" FROM "post"))'
   },
   params: []
 });
